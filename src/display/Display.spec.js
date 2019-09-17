6e01f594671d85924505462498187b467013446a
Min-Huang <minh.huang96@gmail.com>
2019-09-16 18:28:57 -0700

    finished testing display.test.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -10,6 +10,10 @@ test("Display renders correctly", () => {
   render(<Display locked={locked} closed={closed} />);
 });
 
+
+
+//displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise
+//when `locked` or `closed` use the `red-led` class
 test("luck-unluck-info display", () => {
    
    const { getByTestId, rerender } = render(<Display locked={true} />);
@@ -18,13 +22,19 @@ test("luck-unluck-info display", () => {
     expect(getByTestId("luck-unluck-info").textContent).toBe("Unlocked");
     rerender(<Display locked={true} />);
     expect(getByTestId("luck-unluck-info").className).toBe("led red-led");
+    rerender(<Display closed={true} />);
+    expect(getByTestId("open-close-info").className).toBe("led red-led");
 });
+
+//displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise
+//when `unlocked` or `open` use the `green-led` class
 test("open-close-info display", () => {
   const { getByTestId, rerender } = render(<Display closed={true} />);
   expect(getByTestId("open-close-info").textContent).toBe("Closed");
   rerender(<Display closed={false} />);
     expect(getByTestId("open-close-info").textContent).toBe("Open");
-    
+     rerender(<Display locked={false} />);
+     expect(getByTestId("luck-unluck-info").className).toBe("led green-led");
     rerender(<Display closed={false} />);
     expect(getByTestId("open-close-info").className).toBe("led green-led");
     
