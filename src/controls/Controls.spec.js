b0881200eb8bf28a882becc037408931ba547d17
Min-Huang <minh.huang96@gmail.com>
2019-09-16 18:19:43 -0700

    finished testing controls component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,15 +1,14 @@
 // Test away
 import React from "react";
-import { render } from "@testing-library/react";
-import { shallow } from "enzyme";
+import { render, fireEvent } from "@testing-library/react";
 
 import Controls from "./Controls";
 
 test("Controls renders correctly", () => {
-    const toggleClosed = jest.fn();
-    const toggleLocked = jest.fn();
-    const locked = false;
-    const closed = false;
+  const toggleClosed = jest.fn();
+  const toggleLocked = jest.fn();
+  const locked = false;
+  const closed = false;
   render(
     <Controls
       locked={locked}
@@ -20,31 +19,59 @@ test("Controls renders correctly", () => {
   );
 });
 
-test('test open-close-gate button-text-content', () => {
-    const { getByTestId,rerender } = render(<Controls closed={false} />);
-    expect(getByTestId('OpenOrClose-Gate-btn').textContent).toBe("Close Gate");
-     rerender(<Controls closed={true} />);
-     expect(getByTestId("OpenOrClose-Gate-btn").textContent).toBe("Open Gate");
-})
+// test toggle function when click Open-Or-Close-Gate button, locked state = false;
+test("Toggle function is called when Open-Or-Close-Gate buttons click", () => {
+  const toggleClosed = jest.fn();
+
+  const { getByTestId } = render(<Controls toggleClosed={toggleClosed} />);
+
+  const OpenOrCloseGatebtn = getByTestId("OpenOrClose-Gate-btn");
+
+  fireEvent.click(OpenOrCloseGatebtn);
+
+  expect(toggleClosed).toHaveBeenCalled();
+});
+
+// test toggle function when click Locked-Or-Unlocked-Gate button, closed state = true;
+test("Toggle function is called when Locked-Or-Unlocked-Gate buttons click", () => {
+  const toggleLocked = jest.fn();
+  const { getByTestId } = render(
+    <Controls toggleLocked={toggleLocked} closed={true} />
+  );
+  const LockedOrUnlockedGatebtn = getByTestId("LockedOrUnlocked-Gate-btn");
+
+  fireEvent.click(LockedOrUnlockedGatebtn);
+
+  expect(toggleLocked).toHaveBeenCalled();
+});
+
+//buttons' text changes to reflect the state the door will be in if clicked
+test("test open-close-gate button-text-content", () => {
+  const { getByTestId, rerender } = render(<Controls closed={false} />);
+  expect(getByTestId("OpenOrClose-Gate-btn").textContent).toBe("Close Gate");
+  rerender(<Controls closed={true} />);
+  expect(getByTestId("OpenOrClose-Gate-btn").textContent).toBe("Open Gate");
+});
 
 test("test lock-locked-gate button-text-content", () => {
   const { getByTestId, rerender } = render(<Controls locked={false} />);
-  expect(getByTestId("LockedOrUnlocked-Gate-btn").textContent).toBe("Lock Gate");
+  expect(getByTestId("LockedOrUnlocked-Gate-btn").textContent).toBe(
+    "Lock Gate"
+  );
   rerender(<Controls locked={true} />);
-  expect(getByTestId("LockedOrUnlocked-Gate-btn").textContent).toBe("Unlock Gate");
+  expect(getByTestId("LockedOrUnlocked-Gate-btn").textContent).toBe(
+    "Unlock Gate"
+  );
 });
 
+//the closed toggle button is disabled if the gate is locked
 test("test open-close-gate button disabled", () => {
   const { getByTestId } = render(<Controls locked={true} />);
-  expect(getByTestId("OpenOrClose-Gate-btn").props('disabled').toBe(true))
-  
-  
+  expect(getByTestId("OpenOrClose-Gate-btn").disabled).toBe(true);
 });
+
+//the locked toggle button is disabled if the gate is open
 test("test lock-locked-gate button disabled", () => {
   const { getByTestId } = render(<Controls closed={false} />);
-  expect(
-    getByTestId("LockedOrUnlocked-Gate-btn")
-      .props("disabled")
-      .toBe(true)
-  );
+  expect(getByTestId("LockedOrUnlocked-Gate-btn").disabled).toBe(true);
 });
