020aa289718ff6bab12db66ab816a560ac3b1a2f
Min-Huang <minh.huang96@gmail.com>
2019-09-17 08:52:03 -0700

    added one test to dashboard.test.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,8 +3,14 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import Dashboard from './Dashboard'
+import Display from "../display/Display";
 
 test("Dashboard renders correctly", () => {
   render(<Dashboard />);
 });
 
+test("defaults to unlocked and open", () => {
+  const { getByText } = render(<Display />);
+  getByText(/unlocked/i);
+  getByText(/open/i);
+});
